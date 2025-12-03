1247d4988dc92f5a22aee19491d1308e6c2f58cc
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-12-03 12:29:49 -0500

    changed to hono from express

--- backend/controllers/test.controller.ts
+++
@@ -1,9 +0,0 @@
-export function test(req: Request,): Response {
-    const message = "Hello from test con";
-    return new Response(message, {
-        status: 200,
-        headers: {
-            "Content-Type": "text/plain; charset=utf-8"
-        }
-    });
-}
