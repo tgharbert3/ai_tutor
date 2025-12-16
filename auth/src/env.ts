f5dedeab9ae1dbfcce5d515e6a149b60c1fc76ef
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-12-16 08:23:05 -0500

    added local schmea to env.ts to make .env.local type safe

--- auth/src/env.ts
+++ auth/src/env.ts
@@ -1,11 +1,30 @@
+// This file defines a type safe env
+
 import type { ZodError } from "zod";
 
 import { z } from "zod";
 
+const EnvLocalSchema = z.object({
+  DATABASE_URL: z.url(),
+  DATABASE_AUTH_TOKEN: z.string().optional(),
+});
+
+// .extend merges the zod objects
+// .superRefine allows for a more validation and a better error message
 const EnvSchema = z.object({
   ENVIRONMENT: z.string().default("development"),
   PORT: z.coerce.number(),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"]).default("info"),
+}).extend(EnvLocalSchema.shape).superRefine((input, ctx) => {
+  if (input.ENVIRONMENT === "production" && !input.DATABASE_AUTH_TOKEN) {
+    ctx.addIssue({
+      code: "invalid_type",
+      expected: "string",
+      received: "undefined",
+      path: ["DATABASE_AUTH_TOKEN"],
+      message: "Must be set when NODE_ENV is 'production'",
+    });
+  }
 });
 
 export type env = z.infer<typeof EnvSchema>;
