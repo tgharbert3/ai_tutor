d148b4b493be5a5da7b801c82be4a9af1af25148
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-12-16 10:18:29 -0500

    upadated getOne Handler to query the db instead of hardcoded strings

--- auth/src/db/index.ts
+++ auth/src/db/index.ts
@@ -3,10 +3,14 @@ import { drizzle } from "drizzle-orm/libsql";
 
 import env from "@/env.js";
 
+import * as schema from "./schema.js";
+
 const client = createClient({
   url: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
 });
-const db = drizzle({ client });
+const db = drizzle(client, {
+  schema,
+});
 
 export default db;
--- auth/src/routes/users/user.handlers.ts
+++ auth/src/routes/users/user.handlers.ts
@@ -1,9 +1,17 @@
 import type { AppRouteHandler } from "@/lib/types.js";
 
+import db from "@/db/index.js";
+
 import type { GetOneRoute } from "./user.routes.js";
 
-export const getOne: AppRouteHandler<GetOneRoute> = (c) => {
-  return c.json({
-    username: "user 1",
+export const getOne: AppRouteHandler<GetOneRoute> = async (c) => {
+  const { id } = c.req.valid("param");
+  const user = await db.query.users.findFirst({
+    // fields are the columns in the table and the operators are eq, ne, gt, etc...
+    where: (fields, operators) => {
+      return operators.eq(fields.id, id);
+    },
   });
+
+  return c.json(user);
 };
