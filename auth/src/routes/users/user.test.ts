61a828fb10843576938640495f6d66573e1da5ff
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-12-17 19:28:57 -0500

    setup testing using the hono testing client

---
+++ auth/src/routes/users/user.test.ts
@@ -0,0 +1,40 @@
+import { testClient } from "hono/testing";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+import env from "@/env.js";
+import { ZOD_ERROR_MESSAGES } from "@/lib/constants.js";
+import createApp from "@/lib/create-app.js";
+
+import router from "../users/user.index.js";
+
+if (env.ENVIRONMENT !== "test") {
+  throw new Error("Must be in test Environment");
+}
+
+const client = testClient(createApp().route("/", router));
+
+describe("user Routes", () => {
+  beforeAll(async () => {
+    await execSync(`bunx drizzle-kit push`);
+  });
+
+  afterAll(async () => {
+    fs.rmSync("test.db", { force: true });
+  });
+
+  it("get /users validates the body when creating a new user", async () => {
+    const response = await client.users[":id"].$get({
+      param: {
+        id: "wat",
+      },
+    });
+    expect(response.status).toBe(422);
+    if (response.status === 422) {
+      const json = await response.json();
+      expect(json.error.issues[0].path[0]).toBe("id");
+      expect(json.error.issues[0].message).toBe(ZOD_ERROR_MESSAGES.EXPECTED_NUMBER);
+    }
+  });
+});
